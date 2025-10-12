98a730f76d7feeb10a3d2bdedd86d16cf6147484
Arseny Bochkarev <bochkarevarseny@gmail.com>
2025-10-12 20:50:42 +0300

    Add HW1

--- internal/ssa/builder.go
+++ internal/ssa/builder.go
@@ -2,9 +2,16 @@
 package ssa
 
 import (
+	"fmt"
+	"go/ast"
+	"go/importer"
+	"go/parser"
 	"go/token"
+	"go/types"
 
+	"golang.org/x/tools/go/packages"
 	"golang.org/x/tools/go/ssa"
+	"golang.org/x/tools/go/ssa/ssautil"
 )
 
 // Builder отвечает за построение SSA из исходного кода Go
@@ -19,22 +26,42 @@ func NewBuilder() *Builder {
 	}
 }
 
-// TODO: Реализуйте следующие методы в рамках домашнего задания
-
 // ParseAndBuildSSA парсит исходный код Go и создаёт SSA представление
 // Возвращает SSA программу и функцию по имени
 func (b *Builder) ParseAndBuildSSA(source string, funcName string) (*ssa.Function, error) {
-	// TODO: Реализовать
-	// Шаги:
-	// 1. Парсинг исходного кода с помощью go/parser
-	// 2. Создание SSA программы
-	// 3. Поиск нужной функции по имени
+	fset := token.NewFileSet()
 
-	// Подсказки:
-	// - Используйте parser.ParseFile для парсинга
-	// - Создайте packages.Config и загрузите пакет
-	// - Используйте ssautil.CreateProgram для создания SSA
-	// - Найдите функцию в SSA программе
+	file, err := parser.ParseFile(fset, "main.go", source, parser.ParseComments)
+	if err != nil {
+		panic("parser error")
+	}
+	files := []*ast.File{file}
 
-	panic("не реализовано")
+	pkg := types.NewPackage("homework1/main.go", "main")
+
+	lprog, _, err := ssautil.BuildPackage(
+		&types.Config{Importer: importer.Default()}, fset, pkg, files, ssa.SanityCheckFunctions)
+	if err != nil {
+		panic("type error in package")
+	}
+
+	cfg := &packages.Config{Mode: packages.LoadSyntax}
+	initial, err := packages.Load(cfg, "main")
+	if err != nil {
+		panic("error in package load")
+	}
+	prog, _ := ssautil.Packages(initial, ssa.PrintPackages)
+	_ = prog
+	lprog.Build()
+
+	for _, p := range prog.AllPackages() {
+		if fnObj := p.Func(funcName); fnObj != nil {
+			return fnObj, nil
+		}
+	}
+	if fnObj := lprog.Func(funcName); fnObj != nil {
+		return fnObj, nil
+	}
+
+	return nil, fmt.Errorf("function %s not found", funcName)
 }
